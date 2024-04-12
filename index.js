44815ee6679cb200a4801c9cb8f6fc5afd7dea0b
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-12 11:35:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(stringOne, stringTwo){
+    fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json",
+        },
+        body: JSON.stringify({
+            'name' : stringOne, 
+            'email': stringTwo,
+        })
+    })
+    .then(res=>res.json())
+    .then(obj=>{console.log(obj.id)
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+    })
+    return fetch("http://localhost:3000/users")
+    }
+submitData("Steve","steve@steve.com")
